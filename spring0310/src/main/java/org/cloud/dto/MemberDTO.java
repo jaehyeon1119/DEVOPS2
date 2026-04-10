3829046b6d0a097397c4b0642196693de49f498c
jaehyeon1119 <leajae@naver.com>
2026-04-10 10:50:52 +0900

    first commit: spring0310 project

---
+++ spring0310/src/main/java/org/cloud/dto/MemberDTO.java
@@ -0,0 +1,37 @@
+package org.cloud.dto;
+
+import lombok.Data;
+
+@Data
+public class MemberDTO {
+
+	private String userId;
+	private String userPw;
+	private String userName;
+	private String regDate;
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserPw() {
+		return userPw;
+	}
+	public void setUserPw(String userPw) {
+		this.userPw = userPw;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+	
+}
---
+++ spring0310/src/main/java/org/cloud/dto/NoticeDTO.java
@@ -0,0 +1,51 @@
+package org.cloud.dto;
+
+import lombok.Data;
+
+@Data
+public class NoticeDTO {
+
+	private int id;
+	private String title;
+	private String content;
+	private String writer;
+	private String regDate;
+	private int hit;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+	public int getHit() {
+		return hit;
+	}
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+	
+}
